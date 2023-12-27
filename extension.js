865e72a57225666d5cf34928d3162646a7f6e21a
Phyzait Lu <i@phyzait.moe>
2023-12-27 23:58:17 +0000

    update

--- extension.js
+++ extension.js
@@ -23,23 +23,55 @@ async function activate(context) {
     ctx = context
     environment = context.environmentVariableCollection;
     channel = vscode.window.createOutputChannel("nix develop");
-    let e = ctx.workspaceState.get('nixdevelopcache')
-    if (e !== undefined) {
-        updateEnv(e)
-        vscode.window.showInformationMessage('Reloaded with nix environment');
+
+    if (ctx.workspaceState.get('nixdevelopenable')) {
+        let e = ctx.workspaceState.get('nixdevelopcache')
+        if (e !== undefined) {
+            updateEnv(e)
+            vscode.window.showInformationMessage('Reloaded with nix environment');
+        }
     }
 
-    let disposable = vscode.commands.registerCommand('extension.nixdevelop', async function () {
-        try {
-            await storeEnv()
-            await tryReload()
-        } catch (error) {
-            vscode.window.showErrorMessage(`Error: ${error.message}`);
-        }
-    });
+    context.subscriptions.push(
+        vscode.commands.registerCommand('extension.nixdevelopupdate', async function () {
+            try {
+                let nixdevelopenv = await getNixDevelopEnv()
+                if (nixdevelopenv) {
+                    await storeEnv(nixdevelopenv, 'nixdevelopcache')
+                }
+                await tryReload()
+            } catch (error) {
+                vscode.window.showErrorMessage(`Error: ${error.message}`);
+            }
+        })
+    );
 
+    context.subscriptions.push(
+        vscode.commands.registerCommand('extension.nixdevelopenable', async function () {
+            ctx.workspaceState.update('nixdevelopenable', true)
 
-    context.subscriptions.push(disposable);
+            let b = ctx.workspaceState.get('nixdevelopbackup')
+
+            if (b === undefined) {
+                await storeEnv(await getPlainEnv(), 'nixdevelopbackup')
+            }
+
+        })
+    );
+
+    context.subscriptions.push(
+        vscode.commands.registerCommand('extension.nixdevelopdisable', async function () {
+            ctx.workspaceState.update('nixdevelopenable', false)
+
+            let b = ctx.workspaceState.get('nixdevelopbackup')
+            if (b) {
+                restoreProcessEnvWithEnvironment(b)
+                ctx.workspaceState.update('nixdevelopbackup', undefined)
+            }
+
+            ctx.workspaceState.update('nixdevelopcache', undefined)
+        })
+    );
 }
 
 function parseEnv(e) {
@@ -53,22 +85,59 @@ function parseEnv(e) {
     return arr
 }
 
+function getShellEnv() {
+    var env = {}
+    environment.forEach((variable, mutator, collection) => {
+        var result = ""
+        if (mutator.length) {
+            mutator.forEach((type, value) => {
+                switch (type) {
+                    case 1:
+                        result = value
+                    case 2:
+                        result = result + value
+                    case 3:
+                        result = value + result
+                }
+            })
+        } else {
+            result = mutator.value
+        }
+        env[variable] = result
+    });
+}
 
-async function getEnv() {
+
+async function getEnv(cmd) {
     channel.appendLine("Running nix develop")
     return new Promise((resolve, reject) => {
-        exec("nix develop --command bash -c 'env'", {
-            cwd: getWorkingFolder()
+        exec(cmd, {
+            cwd: getWorkingFolder(),
+            // env: getShellEnv()
         }, (error, stdout, stderr) => {
             if (stdout.length > 0) {
                 channel.appendLine("Env parse ok")
                 resolve(stdout)
             } else {
                 channel.appendLine("Env parse fail")
+                channel.append(error)
+                channel.append(stderr)
                 reject(stderr)
             }
         });
     })
+}
+
+function restoreProcessEnvWithEnvironment(e) {
+    for (let key in process.env) {
+        delete process.env[key]
+    }
+    environment.clear()
+    for (const i of e) {
+        const [k, v] = i
+        process.env[k] = v
+        environment.replace(k, v)
+    }
 }
 
 function updateEnv(e) {
@@ -83,10 +152,36 @@ function updateEnv(e) {
     }
 }
 
-async function storeEnv() {
-    let e = await getEnv()
-    let map = parseEnv(e)
-    ctx.workspaceState.update('nixdevelopcache', map)
+const keyValueArray2Dict = (arr) => {
+    arr.reduce((acc, [key, value]) => {
+        acc[key] = value;
+        return acc;
+    }, {})
+}
+
+function restoreEnv(e) {
+    try {
+        for (const i of e) {
+            const [k, v] = i
+            process.env[k] = v
+            environment.replace(k, v)
+        }
+    } catch (err) {
+
+    }
+}
+
+async function getNixDevelopEnv() {
+    return await getEnv("nix develop --command bash -c 'env'")
+}
+
+async function getPlainEnv() {
+    return await getEnv("bash -c 'env'")
+}
+
+async function storeEnv(env, name) {
+    let map = parseEnv(env)
+    ctx.workspaceState.update(name, map)
 }
 
 async function tryReload() {
